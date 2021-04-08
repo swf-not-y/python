37ab854800de65cde65cb45aa25e4ea9912bf3df
swf-not-y <1873366265@qq.com>
2021-04-08 22:47:19 +0800

    Create work4-3.py

---
+++ work4-3.py
@@ -0,0 +1,9 @@
+import random
+
+for i in range(100):
+    print([random.randint(1,10000),random.randint(1,10000)])
+    nums = [random.randint(1,10000),random.randint(1,10000)]
+target = input("请输入一个值：")
+
+
+
